aede840b04d629cbacf06c264a0fc5ac5fca6c66
Peter Georgiev <peter_85@abv.bg>
2017-03-23 15:57:27 +0200

    Preparing for exam

--- Sample Exams/17-June-2014/task-2/task-resources/scripts.js
+++ Sample Exams/17-June-2014/task-2/task-resources/scripts.js
@@ -1,3 +1,47 @@
 /* globals $ */
-$.fn.gallery = function () {
+$.fn.gallery = function(cols) {
+    cols = cols || 4;
+
+    var $this = $(this);
+    $this.addClass("gallery");
+    $selected = $this.children(".selected");
+    $selected.hide();
+    var $galleryList = $(".gallery-list");
+
+    var $imageContainer = $(".image-container");
+    $imageContainer.each(function(index, element) {
+        if (index % cols === 0) {
+            $(element).addClass("clearfix");
+        }
+    });
+
+    $galleryList.on("click", "img", function() {
+        $selected.show();
+    });
+
+
+
+
+    return $this;
+    //creating a table
+    // var $table = $("<table>")
+    //     .addClass("gallery")
+    //     .appendTo($(".gallery-list"));
+    // var rows = Math.ceil(12 / cols);
+    // var counter = 0;
+    // for (var row = 0; row < rows; row++) {
+    //     var $tr = $("<tr>").addClass("row" + row).appendTo($table);
+    //     for (var col = 0; col < cols; col++) {
+
+    //         var $td = $("<td>")
+    //             .addClass("cell" + counter)
+    //             // .append($("[data-info]"))
+    //             // .eq((col + 1) * (row + 1))
+    //             .appendTo($tr);
+    //         var $img = $("[data-info]").eq(counter).appendTo($td);
+    //         counter += 1;
+    //     }
+    //}
+
+
 };
--- Sample Exams/Sample-exam-1/task-2/task-files/scripts.js
+++ Sample Exams/Sample-exam-1/task-2/task-files/scripts.js
@@ -1,3 +1,23 @@
-$.fn.tabs = function () {
+$.fn.tabs = function() {
 
+    var $this = $(this);
+    var $tabItem = $(".tab-item");
+    //initial state
+    $this.addClass("tabs-container");
+    $(".tab-item-content").hide();
+    $(".tab-item-content").eq(1).show();
+    $tabItem.eq(1)
+        .addClass("current");
+
+    //what happend after clicking
+    $tabItem.on("click", function() {
+        let $this = $(this);
+        $(".tab-item-content").hide();
+        $tabItem.removeClass("current");
+        $this.addClass("current");
+        $this.children(".tab-item-content").show();
+    });
+
+
+    // return $this;
 };
